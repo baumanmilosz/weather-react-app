4ac5ccb233a8738d7325efa60243fbc83d643bfb
MBauman <bauman.milosz@gmail.com>
2019-11-07 15:50:50 +0100

    Updates

---
+++ precache-manifest.45a129aa77757cff562d544bba10384b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6f7ae578b284d7bcdeb",
+    "url": "/weather-react-app/static/js/main.e6f7ae57.chunk.js"
+  },
+  {
+    "revision": "e7c041a10672ccb52809",
+    "url": "/weather-react-app/static/js/1.e7c041a1.chunk.js"
+  },
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "7c0960836edceb5b9c6cd5a3dd4f3edb",
+    "url": "/weather-react-app/static/media/bg.7c096083.png"
+  },
+  {
+    "revision": "623c7118249e082fe87a78e08506cb4b",
+    "url": "/weather-react-app/static/media/segoeui.623c7118.ttf"
+  },
+  {
+    "revision": "24ab51f6ae1804c808f822d28b9be18a",
+    "url": "/weather-react-app/static/media/dubai.24ab51f6.ttf"
+  },
+  {
+    "revision": "8c575f90f557fb81afd6b019a92b76e4",
+    "url": "/weather-react-app/static/media/magnifierIcon.8c575f90.svg"
+  },
+  {
+    "revision": "051e123c2444b28b02730bf1821ffcd3",
+    "url": "/weather-react-app/static/media/sunIcon.051e123c.svg"
+  },
+  {
+    "revision": "a0f792644f58b2fb7bf44c8379ea78d9",
+    "url": "/weather-react-app/index.html"
+  }
+];
